67a2aec815942be32bfb2d8f23781b0da7d58da5
Rishav <rishav.jnit@gmail.com>
2018-02-13 13:23:12 +0530

    Change folder name to 4 javascript

---
+++ 4 JavaScript/changeHTMLstyle.js
@@ -0,0 +1,11 @@
+document.getElementById('button1').onclick=function () {
+	document.getElementById('facebook').style.backgroundColor='brown';
+	document.getElementById('facebook').style.color='blue';
+
+}
+document.getElementById('button2').onclick=function(){
+	document.getElementById('orkut').style.width='200px';
+	document.getElementById('orkut').style.fontSize='20px';
+
+
+}
