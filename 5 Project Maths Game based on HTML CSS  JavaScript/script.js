02ddc6e4c3fa178de2c89497efe6d8bf95886119
Rishav <rishav.jnit@gmail.com>
2018-02-13 23:26:52 +0530

    Completed JavaScript Project

--- 5 Project Maths Game based on HTML CSS  JavaScript/script.js
+++ 5 Project Maths Game based on HTML CSS  JavaScript/script.js
@@ -1,7 +1,8 @@
 var playing = false;
-var score = 1;
+var score = 0;
 var action;
 var timeremaining;
+var correctAnswer;
 document.getElementById('startreset').onclick=function () {
 	if(playing==true)
 	{
@@ -17,6 +18,7 @@ document.getElementById('startreset').onclick=function () {
 		document.getElementById('gameOver').style.display='none';
 		document.getElementById('timeremainingvalue').innerHTML=timeremaining;
 		startCounter();
+		generateQA();
 	}
 }
 function startCounter()
@@ -40,4 +42,54 @@ function startCounter()
 function stopCounter()
 {
 	clearInterval(action);
+}
+function generateQA()
+{
+	var x = 1+Math.round(9*Math.random());
+	var y = 1+Math.round(9*Math.random());
+	correctAnswer = x * y ;
+	document.getElementById('question').innerHTML=x+'x'+y;
+	var position = 1+Math.round(3*Math.random());
+	document.getElementById('box'+position).innerHTML=correctAnswer;
+	var answers = [correctAnswer];
+	for(i=1;i<5;i++)
+	{
+		if(i!=position)
+		{
+			var wrongAnswer;
+			do
+			{
+			wrongAnswer=(1+Math.round(9*Math.random()))*(1+Math.round(9*Math.random()));
+			}
+			while(answers.indexOf(wrongAnswer)>-1)
+			document.getElementById('box'+i).innerHTML=wrongAnswer;
+			answers.push(wrongAnswer);
+		}
+	}
+}
+for(i=1;i<5;i++)
+{
+document.getElementById('box'+i).onclick=function()
+{
+	if(playing==true)
+	{
+		if(this.innerHTML==correctAnswer)
+		{
+			score++;
+			document.getElementById('scoreValue').innerHTML=score;
+			document.getElementById('correct').style.display='block';
+			setTimeout(function(){
+				document.getElementById('correct').style.display='none';
+			},1000);
+			generateQA();
+		}
+		else
+		{
+			document.getElementById('wrong').style.display='block';	
+			setTimeout(function(){
+				document.getElementById('wrong').style.display='none';
+			},1000);
+		}
+	}
+}
 }
